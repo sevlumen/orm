0ce9297905649b2ea8f98b0e5749d0debd64993f
Quang <97324003+quang0201@users.noreply.github.com>
2026-08-06 13:11:19 +0700

    Add ormgen version reporting

--- cmd/ormgen/main.go
+++ cmd/ormgen/main.go
@@ -2,12 +2,14 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/sevlumen/orm/generator"
+	"github.com/sevlumen/orm/internal/buildinfo"
 )
 
 type typeList []string
@@ -27,11 +29,25 @@ func main() {
 	dir := flag.String("dir", ".", "package directory containing entity structs")
 	output := flag.String("output", "orm_gen.go", "generated Go file inside the package directory")
 	check := flag.Bool("check", false, "verify generated output without writing")
+	version := flag.Bool("version", false, "print version and build metadata")
+	versionJSON := flag.Bool("version-json", false, "print version metadata as JSON")
 	flag.Var(&types, "type", "exported entity type; repeat the flag or use comma-separated names")
 	flag.Parse()
 	if flag.NArg() != 0 {
 		fmt.Fprintln(os.Stderr, "ormgen: unexpected positional arguments")
 		os.Exit(2)
+	}
+	if *version || *versionJSON {
+		info := buildinfo.Current()
+		if *versionJSON {
+			if err := json.NewEncoder(os.Stdout).Encode(info); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			return
+		}
+		fmt.Printf("ormgen %s commit=%s date=%s dirty=%t go=%s %s/%s\n", info.Version, info.Commit, info.Date, info.Dirty, info.GoVersion, info.GOOS, info.GOARCH)
+		return
 	}
 	if err := generator.Write(generator.Config{Dir: *dir, Types: types, Output: *output, Check: *check}); err != nil {
 		fmt.Fprintln(os.Stderr, err)
