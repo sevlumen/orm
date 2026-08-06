c6c801175620c47a5168cdae4c3a2f4e4840022f
Quang <97324003+quang0201@users.noreply.github.com>
2026-08-06 10:45:11 +0700

    Add atomic generator replacement on Windows

---
+++ generator/replace_windows.go
@@ -0,0 +1,39 @@
+//go:build windows
+
+package generator
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+const (
+	moveFileReplaceExisting = 0x1
+	moveFileWriteThrough     = 0x8
+)
+
+var moveFileExW = syscall.NewLazyDLL("kernel32.dll").NewProc("MoveFileExW")
+
+func replaceFile(source, destination string) error {
+	sourceUTF16, err := syscall.UTF16PtrFromString(source)
+	if err != nil {
+		return fmt.Errorf("encode source path: %w", err)
+	}
+	destinationUTF16, err := syscall.UTF16PtrFromString(destination)
+	if err != nil {
+		return fmt.Errorf("encode destination path: %w", err)
+	}
+	result, _, callErr := moveFileExW.Call(
+		uintptr(unsafe.Pointer(sourceUTF16)),
+		uintptr(unsafe.Pointer(destinationUTF16)),
+		uintptr(moveFileReplaceExisting|moveFileWriteThrough),
+	)
+	if result != 0 {
+		return nil
+	}
+	if callErr != nil && callErr != syscall.Errno(0) {
+		return callErr
+	}
+	return syscall.EINVAL
+}
